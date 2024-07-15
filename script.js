5ed013037d875b7ef3c0bc78d153b89cc6b26a07
busracokal <164003345+busracokal@users.noreply.github.com>
2024-07-16 01:51:20 +0300

    büt

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 //declaring an array
-let number = ["one","two","three","four"];
-console.log(number[0]);//output:"one"
-number.push("five");//adds"five"to the end
-console.log(number);//removes the last element
-number.splice(1,1)
-console.log(number)
 
-let person = {
-    firstName:"Sena",
-    lastName:"Aykız",
-    age:24,
-    hobbies:["reading", "travelling"],
-    adress:{
-        street:"Sinanpaşa",
-        city:"İstanbul",
-        country:"Türkiye"
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    var currentTime = new Date().getHours();
+    var greetingMessage;
+  
+    if (currentTime >= 6 && currentTime < 18) {
+      greetingMessage = "Merhaba";
+    } else {
+      greetingMessage = "İyi Akşamlar";
     }
-};
-//accessing properties
-console.log(person.firstName); //output:"Sena"
+  
+    document.getElementById("greeting").textContent = greetingMessage;
+    
+    // Custom cursor setup
+    var cursor = document.createElement("div");
+    cursor.classList.add("custom-cursor");
+    document.body.appendChild(cursor);
+  
+    document.addEventListener("mousemove", function(e) {
+      cursor.style.left = e.clientX + "px";
+      cursor.style.top = e.clientY + "px";
+    });
+  });
 
-//modifiyng an object
-person.age=25;
-person.hobbies.push("Cooking");
+// script.js
+function setThemeBasedOnTime() {
+    const hour = new Date().getHours();
+    const body = document.body;
+  
+    if (hour >= 6 && hour < 18) {
+      body.classList.add('light');
+      body.classList.remove('dark');
+    } else {
+      body.classList.add('dark');
+      body.classList.remove('light');
+    }
+  }
+  
+  // Sayfa yüklendiğinde temayı ayarla
+  window.onload = setThemeBasedOnTime;
+  
 
-for(let i=0;i<5;i++) {
-    console.log(i);
-}
 
-//Iterating through an array
-for(let i=0; i<number.length; i++) {
-    console.log(number[i]);
-}
 
-//Itearting through object properties
-for (let key in person) {
-    console.log(key + ":" + person[key]);
-}
-let x=10;
-if(x>5) {
-    console.log("grapes")
-}
-else{ 
-    console.log("apple")
-}
 
 let temperature=23;
 if (temperature>20) {
@@ -54,23 +54,4 @@ if(hour<12) {
     console.log("Good Morning!");
 }
 else{ console.log("Good Afternoon!");
-}
-let age =25;
-if (age>=18) {
-    if(age<21) {
-        console.log("You are an adult but not yet allowed to drink.");
-    }
-    else{
-        console.log("You are an adult and allowed to drink.");
-    }
-}
-else{
-    console.log("You are a minor!");
-}
-let movie = 200;
-if(movie>=200) {
-    console.log("Tebrikler, sinefilsin!!");
-}
-else{
-    console.log("Sinefil değilsin, daha fazla film izle!!");
 }
